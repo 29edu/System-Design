15e1f975194c4862142ee6fded553660f1d03e2f
Edison Priyadarshi <edisonpriyadarshi12@gmail.com>
2026-07-01 21:56:12 +0530

    Added Dynamic + Static Polymorphism

--- OOPS/Polymorphism/Polymorphism.java
+++ OOPS/Polymorphism/Polymorphism.java
@@ -1,6 +1,5 @@
-
+// Base Car class
 abstract class Car {
-    
     protected String brand;
     protected String model;
     protected boolean isEngineOn;
@@ -9,99 +8,150 @@ abstract class Car {
     public Car(String brand, String model) {
         this.brand = brand;
         this.model = model;
-        isEngineOn = false;
-        currentSpeed = 0;
+        this.isEngineOn = false;
+        this.currentSpeed = 0;
     }
 
-    // Polymorphsim
-    abstract public void accelerate();
-
+    //Common methods for All cars.
     public void startEngine() {
         isEngineOn = true;
-        System.out.println("The car has started");
-    }
-
-    public void brake() {
-        if(!isEngineOn) {
-            System.out.println(brand + " " + model + ":" +"Engine is off. Brake failed");
-        }
-
-        currentSpeed = 0;
-
-        System.out.println(brand + " " + model + ":" +"Car stopped after applying brake");
+        System.out.println(brand + " " + model + " : Engine started.");
     }
 
     public void stopEngine() {
-        if(!isEngineOn) {
-            System.out.println(brand + " " + model + ":" +"Engine is off. Stop Engine is failed");
-        }
-
         isEngineOn = false;
-        System.out.println(brand + " " + model + " Engine is off now");
+        currentSpeed = 0;
+        System.out.println(brand + " " + model + " : Engine turned off.");
+    }
+
+    public abstract void accelerate();             // Abstract method for Dynamic Polymorphism
+
+    public abstract void accelerate(int speed);    //Abstract method for Static Polymorphism
+    
+    public abstract void brake();                  // Abstract method for Dynamic Polymorphism
+}
+
+class ManualCar extends Car {
+    private int currentGear;
+
+    public ManualCar(String brand, String model) {
+        super(brand, model);
+        this.currentGear = 0;
+    }
+
+    //Specialized method for Manual Car
+    public void shiftGear(int gear) {
+        currentGear = gear;
+        System.out.println(brand + " " + model + " : Shifted to gear " + currentGear);
+    }
+
+    // Overriding accelerate - Dynamic Polymorphism
+    @Override
+    public void accelerate() {
+        if (!isEngineOn) {
+            System.out.println(brand + " " + model + " : Cannot accelerate! Engine is off.");
+            return;
+        }
+        currentSpeed += 20;
+        System.out.println(brand + " " + model + " : Accelerating to " + currentSpeed + " km/h");
+    }
+
+    //overriding and overloading accelerate at the same time.
+    @Override
+    public void accelerate(int speed) {
+        if (!isEngineOn) {
+            System.out.println(brand + " " + model + " : Cannot accelerate! Engine is off.");
+            return;
+        }
+        currentSpeed += speed;
+        System.out.println(brand + " " + model + " : Accelerating to " + currentSpeed + " km/h");
+    }
+
+    // Overriding brake - Dynamic Polymorphism
+    @Override
+    public void brake() {
+        currentSpeed -= 20;
+        if (currentSpeed < 0) currentSpeed = 0;
+        System.out.println(brand + " " + model + " : Braking! Speed is now " + currentSpeed + " km/h");
     }
 }
 
 class ElectricCar extends Car {
-
     private int batteryLevel;
 
     public ElectricCar(String brand, String model) {
         super(brand, model);
+        this.batteryLevel = 100;
     }
 
+    //specialized method for Electric Car
     public void chargeBattery() {
         batteryLevel = 100;
-        System.out.println(brand + " " + model + ": Battery is fully charged 100%");
+        System.out.println(brand + " " + model + " : Battery fully charged!");
     }
 
+    // Overriding accelerate - Dynamic Polymorphism
     @Override
     public void accelerate() {
-        currentSpeed+=20;
-        System.out.println(brand + " " + model + ": Electric Car is accelarated to " + currentSpeed);
+        if (!isEngineOn) {
+            System.out.println(brand + " " + model + " : Cannot accelerate! Engine is off.");
+            return;
+        }
+        if (batteryLevel <= 0) {
+            System.out.println(brand + " " + model + " : Battery dead! Cannot accelerate.");
+            return;
+        }
+        batteryLevel -= 10;
+        currentSpeed += 15;
+        System.out.println(brand + " " + model + " : Accelerating to " + currentSpeed +
+                           " km/h. Battery at " + batteryLevel + "%.");
+    }
+
+    // Overriding accelerate - Dynamic Polymorphism
+    @Override
+    public void accelerate(int speed) {
+        if (!isEngineOn) {
+            System.out.println(brand + " " + model + " : Cannot accelerate! Engine is off.");
+            return;
+        }
+        if (batteryLevel <= 0) {
+            System.out.println(brand + " " + model + " : Battery dead! Cannot accelerate.");
+            return;
+        }
+        batteryLevel -= 10 + speed;
+        currentSpeed += speed;
+        System.out.println(brand + " " + model + " : Accelerating to " + currentSpeed +
+                           " km/h. Battery at " + batteryLevel + "%.");
+    }
+
+    // Overriding brake - Dynamic Polymorphism
+    @Override
+    public void brake() {
+        currentSpeed -= 15;
+        if (currentSpeed < 0) currentSpeed = 0;
+        System.out.println(brand + " " + model +
+                           " : Regenerative braking! Speed is now " + currentSpeed +
+                           " km/h. Battery at " + batteryLevel + "%.");
     }
 }
 
-class ManualCar extends Car {
-
-    private int currentGear;
-
-    public ManualCar(String brand, String model) {
-        super(brand, model); // Calls parent class constrcutor
-    }
-
-    public void shiftGear(int gear) {
-        currentGear = gear;
-        System.out.println(brand + " " + model + ": Shifted to gear " + currentGear);
-    }
-
-    @Override
-    public void accelerate() {
-        currentSpeed+=50;
-        System.out.println(brand + " " + model + ": Manual is accelarated to " + currentSpeed);
-    }
-}
-
-
+// Main function
 public class Polymorphism {
     public static void main(String[] args) {
-        
-        ManualCar car = new ManualCar("Ford", "Mustang");
+        Car myManualCar = new ManualCar("Ford", "Mustang");
+        myManualCar.startEngine();
+        myManualCar.accelerate();
+        myManualCar.accelerate();
+        myManualCar.brake();
+        myManualCar.stopEngine();
 
-        car.startEngine();
-        car.shiftGear(1);
-        car.accelerate();
-        car.shiftGear(2);
-        car.accelerate();
-        car.brake();
-        car.stopEngine();
+        System.out.println("----------------------");
 
-        ElectricCar car2 = new ElectricCar("Tesla", "x");
-
-        car2.startEngine();
-        car2.chargeBattery();
-        car2.accelerate();
-        car2.accelerate();
-        car2.brake();
-        car2.stopEngine();
+        Car myElectricCar = new ElectricCar("Tesla", "Model S");
+        myElectricCar.startEngine();
+        myElectricCar.accelerate();
+        myElectricCar.accelerate();
+        myElectricCar.brake();
+        myElectricCar.stopEngine();
     }
 }
